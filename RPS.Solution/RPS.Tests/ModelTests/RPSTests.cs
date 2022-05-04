10f50054f7c63d110b53da13d37092dc0dbab4f8
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-04 14:03:23 -0700

    Good Pass on 8 tests.
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>

--- RPS.Solution/RPS.Tests/ModelTests/RPSTests.cs
+++ RPS.Solution/RPS.Tests/ModelTests/RPSTests.cs
@@ -7,16 +7,102 @@ namespace RPS.Tests
   public class RockPSTests
   {
     [TestMethod]
-    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    public void RockPS_ConstructsGame_strings()
     {
-
-      // Arrange
-
+      string userOneInput = "rock";
+      string userTwoInput = "rock";
+      RockPS game = new RockPS(userOneInput, userTwoInput);
       // Act
-
       // Assert
     
-    Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    Assert.AreEqual(game.PlayerOne, userOneInput);
+    }
+
+    [TestMethod]
+    public void CompareInputs_CheckForDraw_String()
+    {
+      string userOneInput = "rock";
+      string userTwoInput = "rock";
+      RockPS game = new RockPS(userOneInput, userTwoInput);
+      // Act
+      string result = game.CompareInputs();
+      // Assert
+    
+    Assert.AreEqual("Draw", result);
+    }
+    
+    [TestMethod]
+    public void CompareInputs_CheckForWinner_String()
+    {
+      string userOneInput = "rock";
+      string userTwoInput = "paper";
+      RockPS game = new RockPS(userOneInput, userTwoInput);
+      // Act
+      string result = game.CompareInputs();
+      // Assert
+    
+    Assert.AreEqual("Winner Player 2", result);
+    }
+    
+    [TestMethod]
+    public void CompareInputs_CheckForWinner1_String()
+    {
+      string userOneInput = "paper";
+      string userTwoInput = "rock";
+      RockPS game = new RockPS(userOneInput, userTwoInput);
+      // Act
+      string result = game.CompareInputs();
+      // Assert
+    
+    Assert.AreEqual("Winner Player 1", result);
+    }
+    [TestMethod]
+    public void CompareInputs_CheckForWinner3_String()
+    {
+      string userOneInput = "paper";
+      string userTwoInput = "scissors";
+      RockPS game = new RockPS(userOneInput, userTwoInput);
+      // Act
+      string result = game.CompareInputs();
+      // Assert
+    
+    Assert.AreEqual("Winner Player 2", result);
+    }
+    [TestMethod]
+    public void CompareInputs_CheckForWinner4_String()
+    {
+      string userOneInput = "scissors";
+      string userTwoInput = "paper";
+      RockPS game = new RockPS(userOneInput, userTwoInput);
+      // Act
+      string result = game.CompareInputs();
+      // Assert
+    
+    Assert.AreEqual("Winner Player 1", result);
+    }
+    [TestMethod]
+    public void CompareInputs_CheckForWinner5_String()
+    {
+      string userOneInput = "scissors";
+      string userTwoInput = "rock";
+      RockPS game = new RockPS(userOneInput, userTwoInput);
+      // Act
+      string result = game.CompareInputs();
+      // Assert
+    
+    Assert.AreEqual("Winner Player 2", result);
+    }
+     [TestMethod]
+    public void CompareInputs_CheckForWinner6_String()
+    {
+      string userOneInput = "rock";
+      string userTwoInput = "scissors";
+      RockPS game = new RockPS(userOneInput, userTwoInput);
+      // Act
+      string result = game.CompareInputs();
+      // Assert
+    
+    Assert.AreEqual("Winner Player 1", result);
     }
   }
 }
