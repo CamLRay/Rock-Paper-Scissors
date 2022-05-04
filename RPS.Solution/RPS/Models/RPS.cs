82e89c9367794b54c231fa263d2ab23ef61932a5
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-04 14:04:06 -0700

    Logic complete to match tests.
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>

--- RPS.Solution/RPS/Models/RPS.cs
+++ RPS.Solution/RPS/Models/RPS.cs
@@ -2,6 +2,47 @@ namespace RPS
 {
   public class RockPS
   {
-    // properties, constructors, methods, etc. go here
+    public string PlayerOne {get; set;}
+    public string PlayerTwo {get; set;}
+    public RockPS(string playerOneInput, string playerTwoInput)
+    {
+      PlayerOne = playerOneInput;
+      PlayerTwo = playerTwoInput;
+    }
+    public string CompareInputs ()
+    {
+      if (PlayerOne == PlayerTwo)
+      {
+        return "Draw";
+      }
+      else if (PlayerOne == "rock" && PlayerTwo == "paper")
+      {
+        return "Winner Player 2";
+      }
+      else if (PlayerOne == "paper" && PlayerTwo == "rock")
+      {
+        return "Winner Player 1";
+      }
+      else if (PlayerOne == "paper" && PlayerTwo == "scissors")
+      {
+        return "Winner Player 2";
+      }
+      else if (PlayerOne == "scissors" && PlayerTwo == "paper")
+      {
+        return "Winner Player 1";
+      }
+      else if (PlayerOne == "scissors" && PlayerTwo == "rock")
+      {
+        return "Winner Player 2";
+      }
+      else if (PlayerOne == "rock" && PlayerTwo == "scissors")
+      {
+        return "Winner Player 1";
+      }
+      else
+      {
+        return "check more";
+      }
+    }
   }
 }
